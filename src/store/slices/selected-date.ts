3378fb23202d096777c2317f4471ba9d2c278858
ruben <51822812+Ruben-030398@users.noreply.github.com>
2023-09-27 11:00:31 +0400

    fix typo

--- src/store/slices/selected-date.ts
+++ src/store/slices/selected-date.ts
@@ -2,7 +2,7 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit"
 
 const selectedDateSlice = createSlice({
   name: 'selectedDate',
-  initialState: { value: 'l2' },
+  initialState: { value: '' },
   reducers: {
     setSelectedDate: (state, action: PayloadAction<string>) => {
       state.value = action.payload
